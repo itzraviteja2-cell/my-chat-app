47035a84121d984b964d014e1b8df9e0a99ef9f1
itzraviteja2-cell <itzraviteja2@gmail.com>
2026-08-13 23:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ try:
     genai.configure(api_key=api_key)
     model = genai.GenerativeModel('gemini-1.5-flash')
 except Exception as e:
-    st.error(f"API Key సెటప్ లోపం: {e}")
+        st.error(f"API Key సెటప్ లోపం: {e}")
     st.stop()
 
 st.title("✨ Aurora AI")
