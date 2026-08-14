3383a96a2035d15e36a329f5a75e537aa917d2c0
itzraviteja2-cell <itzraviteja2@gmail.com>
2026-08-14 08:51:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ genai.configure(api_key=api_key)
 
 # Initialize Model
 try:
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.5-flash-latest')
 except Exception as e:
     st.error(f"మోడల్ లోడ్ చేయడంలో లోపం జరిగింది: {e}")
     st.stop()
