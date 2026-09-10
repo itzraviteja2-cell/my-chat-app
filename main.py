f1255974c96db43b338aa100c4a9198198e26d2c
itzraviteja2-cell <itzraviteja2@gmail.com>
2026-09-10 21:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,15 +211,8 @@ def chat(request: ChatRequest):
             try:
 
                 response = client.models.generate_content(
-                    model="gemini-3.6-flash",
-                    contents=contents,
-                    config=types.GenerateContentConfig(
-                        tools=[
-                            types.Tool(
-                                google_search=types.GoogleSearch()
-                            )
-                        ]
-                    )
+    model="gemini-3.6-flash",
+    contents=contents
                 )
 
                 break
