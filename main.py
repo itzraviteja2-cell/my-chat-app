e6a225291d93ae53cdda51db82e078d8d2bb3dce
itzraviteja2-cell <itzraviteja2@gmail.com>
2026-09-25 14:46:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,14 @@ app = FastAPI(
     version="2.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # =========================================
 # BASE DIRECTORY
